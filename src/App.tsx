52d84bc3a9a56635997137b1239b0cdef32fd110
Justin <begleyj1@icloud.com>
2023-06-11 18:28:08 -0400

    Removing Contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { About, Intro, Navbar, NotFound, Projects, Posts, Contact} from './components/Layout'
+import { About, Intro, Navbar, NotFound, Projects, Posts} from './components/Layout'
 import './App.css'
 
 const App: React.FC = () => {
