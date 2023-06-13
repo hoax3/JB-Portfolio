d6d86a79eba6081568c66733c84965cf2b9b9ee0
Justin <begleyj1@icloud.com>
2023-06-13 10:44:57 -0400

    im a moron

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -14,12 +14,12 @@ export const Posts = () => {
           <p>Getting started with deploying a full stack app on AWS Amplify</p>
       </Link>
       <Link to={"/coming"} target='_blank' className="posts-card">
-          <img src='/public/wikijs.jpeg' width='170px' height={'140px'} style={{borderRadius:'5px'}}></img>
+          <img src='/wikijs.jpeg' width='170px' height={'140px'} style={{borderRadius:'5px'}}></img>
           <h3>WikiJs</h3>
           <p>Deploying WikiJs on Portainer</p>
       </Link>
       <Link to={"https://github.com/hoax3/Splunk"} target='_blank' className="posts-card">
-          <img src='/public/splunk-img.jpeg' width='170px' height={'100px'} style={{borderRadius:'5px'}}></img>
+          <img src='/splunk-img.jpeg' width='170px' height={'100px'} style={{borderRadius:'5px'}}></img>
           <h3>Splunk Configs</h3>
           <p>Splunk configs to help get you off the ground and start monitoring your environment.</p>
       </Link>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,7 +10,7 @@ export const Projects = () => {
             </h1>
             <div className="projects-card-main">
             <Link to={"https://github.com/hoax3/JB-Portfolio"} target='_blank' className="projects-card">
-                <img src='../../public/screenshot-page.png' width='170px' height={'110px'} style={{borderRadius:'10px'}}></img>
+                <img src='/screenshot-page.png' width='170px' height={'110px'} style={{borderRadius:'10px'}}></img>
                 <h3>Portfolio</h3>
                 <p>My first project using React with TypeScript.</p>
             </Link>
