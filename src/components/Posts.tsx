195445da4d74081e3cd339a29923a338912870a5
Justin <begleyj1@icloud.com>
2023-06-13 10:34:18 -0400

    updating public images

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -9,17 +9,17 @@ export const Posts = () => {
             </h1>
       <div className='posts-card-main'>
       <Link to={"/coming"} target='_blank' className="posts-card">
-          <img src='../../public/amplify.png' width='170px' height={'120px'} style={{borderRadius:'5px'}}></img>
+          <img src='/amplify.png' width='170px' height={'120px'} style={{borderRadius:'5px'}}></img>
           <h3>AWS Amplify</h3>
           <p>Getting started with deploying a full stack app on AWS Amplify</p>
       </Link>
       <Link to={"/coming"} target='_blank' className="posts-card">
-          <img src='../../public/wikijs.jpeg' width='170px' height={'140px'} style={{borderRadius:'5px'}}></img>
+          <img src='/public/wikijs.jpeg' width='170px' height={'140px'} style={{borderRadius:'5px'}}></img>
           <h3>WikiJs</h3>
           <p>Deploying WikiJs on Portainer</p>
       </Link>
       <Link to={"https://github.com/hoax3/Splunk"} target='_blank' className="posts-card">
-          <img src='../../public/splunk-img.jpeg' width='170px' height={'100px'} style={{borderRadius:'5px'}}></img>
+          <img src='/public/splunk-img.jpeg' width='170px' height={'100px'} style={{borderRadius:'5px'}}></img>
           <h3>Splunk Configs</h3>
           <p>Splunk configs to help get you off the ground and start monitoring your environment.</p>
       </Link>
