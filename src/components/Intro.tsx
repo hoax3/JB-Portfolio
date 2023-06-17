347ffa64ebf059a21d1e42178a9c2b770ce150a6
Justin <begleyj1@icloud.com>
2023-06-17 09:40:23 -0400

    fancy blinking to intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,31 +3,34 @@ import { Link } from 'react-router-dom'
 import './styles/intro.css'
 
 export const Intro = () => {
-    return (
-        <div className='container'>
-                <div className="intro-main">
-                    <h1>Hi, I'm Justin </h1>
-                    <h3>[ Engineer | Developer | Tinkerer ]</h3>
-                <div>
-                    <ul className="intro-icons">
-                        <li>
-                            <Link to="mailto:begleyj1@icloud.com" target='_blank'><RiMailLine /></Link>
-                        </li>
-                        <li>
-                            <Link to="https://github.com/hoax3" target='_blank'><RiGithubLine /></Link>
-                        </li>
-                        <li>
-                            <Link to="https://www.linkedin.com/in/jbegley/" target='_blank'><RiLinkedinLine /></Link>
-                        </li>
-                        <li>
-                            <Link to="https://twitter.com/cybersecjust" target='_blank'><RiTwitterLine /></Link>
-                        </li>
-                        <li>
-                            <Link to="https://www.instagram.com/mrbegley3/" target='_blank'><RiInstagramLine /></Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className='container'>
+      <div className="intro-main">
+        <h1>Hi, I'm Justin </h1>
+        <h3>[ Engineer | Developer | Tinkerer ]</h3>
+        <div>
+          <ul className="intro-icons">
+            <li>
+              <Link to="mailto:begleyj1@icloud.com" target='_blank'><RiMailLine /></Link>
+            </li>
+            <li>
+              <Link to="https://github.com/hoax3" target='_blank'><RiGithubLine /></Link>
+            </li>
+            <li>
+              <Link to="https://www.linkedin.com/in/jbegley/" target='_blank'><RiLinkedinLine /></Link>
+            </li>
+            <li>
+              <Link to="https://twitter.com/cybersecjust" target='_blank'><RiTwitterLine /></Link>
+            </li>
+            <li>
+              <Link to="https://www.instagram.com/mrbegley3/" target='_blank'><RiInstagramLine /></Link>
+            </li>
+          </ul>
         </div>
-    )
+        <div className='quick-about'>
+          <Link to={"/about"}>Click here to know more &gt;&gt;</Link>
+        </div>
+      </div>
+    </div>
+  )
 }
